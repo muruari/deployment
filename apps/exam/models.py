686626d016997c9be4ce70413446ab22980a63b9
Robert Murua <muruari@gmail.com>
2018-04-05 14:29:27 -0700

    initial commit

---
+++ apps/exam/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+from django.db import models
+import re, urllib, arithmetic
+
+
+
+
+
+
+
+
+
+class MathDojo(models.Model):
+	def __init__(self)
+
+	def add (self, x, y, *args)
+
+	
+		return x + y + args
+
+	def subtract (self, x, y, *args)
+
+		return  x - y - args
+
+
+
+md = MathDojo()
+
+md.add(1,2,3,4)
